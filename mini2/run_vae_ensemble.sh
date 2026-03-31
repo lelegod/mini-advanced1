9337db4fd3ab9d21452b7f6be42145a249c3f166
lelegod <kylenathanya@gmail.com>
2026-03-31 21:31:07 +0200

    new cov_plot

--- mini2/run_vae_ensemble.sh
+++ mini2/run_vae_ensemble.sh
@@ -2,6 +2,7 @@
 #BSUB -q gpuv100
 #BSUB -J vae_geodesics
 #BSUB -n 4
+#BSUB -R "span[hosts=1]"
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 04:00
 #BSUB -R "rusage[mem=8GB]"
@@ -10,18 +11,24 @@
 #BSUB -o output_%J.out
 #BSUB -e error_%J.err
 
-source ~/miniconda3/etc/profile.d/conda.sh
+source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate advanced_machine_learning
 
 python mini2/geodesics/ensemble_vae.py geodesics \
-    --experiment-folder "experiment" \
+    --experiment-folder "mini2/geodesics/experiment_partA" \
     --device cuda \
     --num-curves 25
 
+python mini2/geodesics/ensemble_vae.py geodesics_ensemble \
+    --experiment-folder "mini2/geodesics/partB" \
+    --device cuda \
+    --num-decoders 10 \
+    --num-curves 25
+
 python mini2/geodesics/ensemble_vae.py evaluate_cov \
-    --experiment-folder "experiment" \
+    --experiment-folder "mini2/geodesics/experiment" \
     --device cuda \
     --num-reruns 10 \
-    --num-decoders 3 \
-    --num-curves 10 \
+    --num-decoders 10 \
+    --num-curves 15 \
     --epochs-per-decoder 50
